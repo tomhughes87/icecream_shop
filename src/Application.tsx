8bd47a39f625b566583d0a3c542544abb9977863
Tom Hughes <tomhughes1987@gmail.com>
2022-09-27 01:13:03 +0200

    place footer compo outside of the router

--- src/Application.tsx
+++ src/Application.tsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Footer from './components/Footer'
 import About from './pages/About'
 import Home from './pages/Home'
 
 export default function Application() {
   return (
+<>
     <BrowserRouter>
         <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='/about' element={<About/>}/>
 
         </Routes>
-        
-
     </BrowserRouter>
-    
+    <Footer/>
+    </>  
 
   )
 }
