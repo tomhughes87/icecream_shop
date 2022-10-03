b382dff0cf24b2b0dbf22d9708f716fa61969072
Tom Hughes <tomhughes1987@gmail.com>
2022-10-03 16:22:33 +0200

    add some endpoints + handle 'pagenotfound'  router

--- src/Application.tsx
+++ src/Application.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Footer from './components/Footer'
 import About from './pages/About'
 import Home from './pages/Home'
+import PageNotFound from './pages/PageNotFound'
 
 export default function Application() {
   return (
@@ -10,7 +11,14 @@ export default function Application() {
     <BrowserRouter>
         <Routes>
             <Route path='/' element={<Home/>}/>
+
             <Route path='/about' element={<About/>}/>
+            <Route path='/About' element={<About/>}/>
+
+            {/* <Route path='/Store' element={<Store/>}/>
+            <Route path='/store' element={<Store/>}/> */}
+
+            <Route path='/*' element={<PageNotFound/>}/>
 
         </Routes>
     </BrowserRouter>
