058dfc7f6691bb47d868581ad50f98573d2f9fb6
Tom Hughes <tomhughes1987@gmail.com>
2022-10-04 00:57:03 +0200

    fix notification bar formating

--- src/components/NotificationBarParent/NotificationBar/index.tsx
+++ src/components/NotificationBarParent/NotificationBar/index.tsx
@@ -1,27 +1,21 @@
 import React from "react";
 
 interface propTypes {
-  shown: boolean,
-  close: any
+  shown: boolean;
+  close: any;
 }
 
-export default function NotificationBar({shown,close}:propTypes) {
+export default function NotificationBar({ shown, close }: propTypes) {
   return shown ? (
-
     <>
-        <div id="NotificationMainContainer">
         <div id="NotificationBarContainer">
           <p id="NotificationBarText">
             Refer a friend and get 25% off your next order!
           </p>
         </div>
-        <div 
-          id="NotificationBarCloseBtn" 
-          onClick={close}
-          >
+        <div id="NotificationBarCloseBtn" onClick={close}>
           <p>x</p>
         </div>
-      </div>
     </>
-    ) : null;
+  ) : null;
 }
