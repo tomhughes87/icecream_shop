111229c7bac5880d6e34030cf0ae67ac66e43bc8
Tom Hughes <tomhughes1987@gmail.com>
2022-10-03 14:02:36 +0200

    link index html to css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
+import './index.css';
 import App from './Application';
 import reportWebVitals from './reportWebVitals';
 
