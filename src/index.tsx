9a50eb1270de2230afd8422e675c89b9cc8f289f
Tom Hughes <tomhughes1987@gmail.com>
2022-10-05 16:05:24 +0200

    css colour pallete

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './Colours.css'
 import App from './Application';
 import reportWebVitals from './reportWebVitals';
 
